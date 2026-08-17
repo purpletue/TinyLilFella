4899bd29ac4dc64ad12cf375c2d4972158de4a97
Uri Yermol <purpletuesdayofficial@gmail.com>
2026-08-17 12:58:00 +0300

    Create switch.cpp

---
+++ examples/switch.cpp
@@ -0,0 +1,3 @@
+#include <Arduino.h>
+// A demonstration of the programmable button at PA6. 
+// This simple program will read the input and if the button is LOW, it'll turn the LED on or off at white.
