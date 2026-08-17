ec8b405b04e4778c60c31b7ead03a8feb4d72f32
Uri Yermol <purpletuesdayofficial@gmail.com>
2026-08-17 13:05:58 +0300

    Implement LED control based on button state
    This program reads the state of a button and controls three LEDs based on the button's state.

--- examples/switch.cpp
+++ examples/switch.cpp
@@ -1,3 +1,24 @@
 #include <Arduino.h>
 // A demonstration of the programmable button at PA6. 
 // This simple program will read the input and if the button is LOW, it'll turn the LED on or off at white.
+#define SWC PA6
+#define BLUE PC15
+#define GREEN PB3
+#define RED PB7
+void setup(){
+  pinMode(SWC, INPUT_PULLUP);
+  pinMode(BLUE, OUTPUT);
+  pinMode(GREEN, OUTPUT);
+  pinMode(RED, OUTPUT);
+} void loop(){
+  int swcState = digitalRead(SWC);
+  if (swcState == LOW) {
+    digitalWrite(BLUE, LOW);
+    digitalWrite(GREEN, LOW);
+    digitalWrite(RED, LOW);
+  } else{
+    digitalWrite(BLUE, HIGH);
+    digitalWrite(GREEN, HIGH);
+    digitalWrite(RED, HIGH);
+  } delay(50);
+}
